afb1c195f9be2949eab105bbd4efae49e6ecac23
Frank <kamunyuz@gmail.com>
2023-11-01 15:23:03 +0300

    betty corrections

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,15 +1,15 @@
 #include "binary_trees.h"
-/*
- * binary_tree_delete - deletes a tree
- * @tree: a pointer to the tree to be deleted
- */
+/**
+* binary_tree_delete - deletes a tree
+* @tree: a pointer to the tree to be deleted
+*/
 
 void binary_tree_delete(binary_tree_t *tree)
 {
-    if (!tree)
-        return;
+	if (!tree)
+		return;
 
-    binary_tree_delete(tree->right);
-    binary_tree_delete(tree->left);
-    free(tree);
+	binary_tree_delete(tree->right);
+	binary_tree_delete(tree->left);
+	free(tree);
 }
